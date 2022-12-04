f5b076985ae2eceae19eef49b70e804866a64776
Egor Ezhkov <egorkashpak21@yandex.ru>
2022-12-04 15:00:36 +0300

    editPopup

--- script.js
+++ script.js
@@ -5,8 +5,36 @@ editButton.addEventListener('click', function() {
   popup.classList.add('popup_opened')
 });
 
+
 let popupClose = document.querySelector('.popup__close');
 
 popupClose.addEventListener('click', function() {
   popup.classList.remove('popup_opened')
+});
+
+let popupInputs = document.querySelectorAll('.popup__input');
+let profileName = document.querySelector('.profile__name');
+let profileProfession = document.querySelector('.profile__profession')
+
+
+popupInputs[0].value = profileName.textContent;
+popupInputs[1].value = profileProfession.textContent;
+
+
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__form');
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+
+    profileName.textContent = popupInputs[0].value;
+    profileProfession.textContent = popupInputs[1].value;
+
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
+
+let popupSubmit = document.querySelector('.popup__submit');
+popupSubmit.addEventListener('click', function() {
+  popup.classList.remove('popup_opened');
 })
