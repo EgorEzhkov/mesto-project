764d279bb4fb51e647a7408b0d30e9b848d5486a
Yaroslav Shadrin <yaroslavbrok@gmail.com>
2023-04-14 17:50:02 +0300

    Feat: Создан класс Api, который выполняет роль подключаемого апи к сайту. Создан общий метод request, который возвращает fetch с двумя параметрами. Первый параметр - это ссылка, второй параметр - это настройки ввиде объекта.

---
+++ src/components/classes/Api.js
@@ -0,0 +1,88 @@
+export const trashRemove = true;
+
+const config = {
+  server: 'https://nomoreparties.co/v1/plus-cohort-22/',
+  headers: {
+    authorization: 'd49dae7b-52fd-4787-ad1c-63454d12ebd1',
+    'Content-Type': 'application/json'
+  }
+};
+
+class Api {
+  constructor(config, checkResponse) {
+    this.server = config.server;
+    this.headers = config.headers;
+    this.checkResponse = checkResponse;
+  }
+
+  request(server, settings) {
+    return fetch(server, settings).then(this.checkResponse);
+  }
+
+  getProfileInfo() {
+    this.request(this.server + 'users/me', {
+      method: 'GET',
+      headers: this.headers
+    });
+  }
+
+  getCardsForServer() {
+    this.request(this.server + 'cards', {
+      method: 'GET',
+      headers: this.headers
+    });
+  }
+
+  editProfileInfo(name, about) {
+    this.request(this.server + 'users/me', {
+      method: 'PATCH',
+      headers: this.headers,
+      body: JSON.stringify({
+        name: name,
+        about: about
+      })
+    });
+  }
+
+  editProfileAvatar(link) {
+    this.request(this.server + 'users/me/avatar', {
+      method: 'PATCH',
+      headers: this.headers,
+      body: JSON.stringify({
+        avatar: link
+      })
+    });
+  }
+
+  addCard(name, link) {
+    this.request(this.server + 'cards', {
+      method: 'POST',
+      headers: this.headers,
+      body: JSON.stringify({
+        name: name,
+        link: link
+      })
+    });
+  }
+
+  deleteCard(id) {
+    this.request(this.server + 'cards/' + `${id}`, {
+      method: 'DELETE',
+      headers: this.headers
+    });
+  }
+
+  addLike(id) {
+    this.request(this.server + 'cards/likes/' + `${id}`, {
+      method: 'PUT',
+      headers: this.headers
+    });
+  }
+
+  deleteLike(id) {
+    this.request(this.server + 'cards/likes/' + `${id}`, {
+      method: 'DELETE',
+      headers: this.headers
+    });
+  }
+}
