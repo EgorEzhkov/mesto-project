6e58de9b50ceb4bc68aaaaba81ab61c3d00cf7b1
Egor Ezhkov <egorkashpak21@yandex.ru>
2023-04-21 01:46:50 +0300

    refactoring

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,16 +1,16 @@
 export default class Popup {
   constructor(selector) {
-    this._selector = selector;
-    this._popup = document.getElementById(this.selector)
+    this.selector = selector;
+    this.popup = document.getElementById(this.selector)
   }
   open() {
-    this._popup.classList.add('popup_opened');
+    this.popup.classList.add('popup_opened');
     document.addEventListener('keydown', (evt) => {
       this._handleEscClose(evt)
     });
   }
   close() {
-    this._popup.classList.remove('popup_opened');
+    this.popup.classList.remove('popup_opened');
     document.removeEventListener('keydown', (evt) => {
       this._handleEscClose(evt)
     });
@@ -23,11 +23,11 @@ export default class Popup {
   }
 
   setEventListeners() {
-    this._popupCloseButton = this._popup.querySelector('.popup__close');
-    this._popupCloseButton.addEventListener('click', () => {
+    this.popupCloseButton = this.popup.querySelector('.popup__close');
+    this.popupCloseButton.addEventListener('click', () => {
       this.close();
     })
-    this._popup.addEventListener('click', (evt) => {
+    this.popup.addEventListener('click', (evt) => {
       if (evt.currentTarget === evt.target) {
         this.close();
       }
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,25 +1,25 @@
 export default class UserInfo {
   constructor(nameSelector, aboutSelector, avatarSelector) {
-    this._nameSelector = document.querySelector(nameSelector) ;
-    this._aboutSelector = document.querySelector(aboutSelector);
-    this._avatarSelector = document.querySelector(avatarSelector);
+    this.nameSelector = document.querySelector(nameSelector) ;
+    this.aboutSelector = document.querySelector(aboutSelector);
+    this.avatarSelector = document.querySelector(avatarSelector);
   }
 
   getUserInfo() {
     return {
-      name: this._nameSelector.textContent,
-      about: this._aboutSelector.textContent,
-      avatar: this._avatarSelector.src
+      name: this.nameSelector.textContent,
+      about: this.aboutSelector.textContent,
+      avatar: this.avatarSelector.src
     }
   }
 
   setUserInfo(value) {
-    this._nameSelector.textContent = value.name;
-    this._aboutSelector.textContent = value.about;
+    this.nameSelector.textContent = value.name;
+    this.aboutSelector.textContent = value.about;
 
   }
 
   setUserAvatar(value) {
-    this._avatarSelector.src = value.avatar;
+    this.avatarSelector.src = value.avatar;
   }
 }
