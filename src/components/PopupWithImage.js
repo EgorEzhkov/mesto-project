667997f1f2835462bed21ac00fb25addda804a2c
Egor Ezhkov <egorkashpak21@yandex.ru>
2023-04-25 13:11:30 +0300

    refactoring popup

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -9,9 +9,9 @@ export default class PopupWithImage extends Popup {
   }
 
   open() {
+    super.open();
     this._popup.querySelector('.popup__image').src = this._image;
     this._popup.querySelector('.popup__image').alt = this._text;
     this._popup.querySelector('.popup__caption').textContent = this._text;
-    super.open();
   }
 }
